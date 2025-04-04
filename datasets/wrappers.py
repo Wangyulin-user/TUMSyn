de0914318b1f9396b45eb7add2b27cfe6d3010dc
kellyWang <57870393+Wangyulin-user@users.noreply.github.com>
2025-04-04 17:31:12 +0800

    Update wrappers.py

--- datasets/wrappers.py
+++ datasets/wrappers.py
@@ -22,21 +22,21 @@ class SRImplicitPaired(Dataset):
 
     def __getitem__(self, idx):
         patch_src_hr, patch_tgt_hr, seq_src, seq_tgt = self.dataset[idx]
-        patch_src_hr = utils.percentile_clip(patch_src_hr)
+        patch_src_hr = utils.percentile_clip(patch_src_hr)  # Image normalization
         patch_tgt_hr = utils.percentile_clip(patch_tgt_hr)
-        non_zero = np.nonzero(patch_src_hr)
+        non_zero = np.nonzero(patch_src_hr) # Remove slices that are completely black
         min_indice = np.min(non_zero, axis=1)
         max_indice = np.max(non_zero, axis=1)
         patch_src_hr = patch_src_hr[min_indice[0]:max_indice[0]+1, min_indice[1]:max_indice[1]+1, min_indice[2]:max_indice[2]+1]
         patch_tgt_hr = patch_tgt_hr[min_indice[0]:max_indice[0]+1, min_indice[1]:max_indice[1]+1, min_indice[2]:max_indice[2]+1]
-        s = np.round(random.uniform(self.scale_min, self.scale_max), 1)
+        s = np.round(random.uniform(self.scale_min, self.scale_max), 1) #Extract 3D patches from the full volume; patch sizes range from 20×20×20 to 60×60×60
         size = (20 * s).astype(int)
         h0 = random.randint(0, patch_src_hr.shape[0] - size)
         w0 = random.randint(0, patch_src_hr.shape[1] - size)
         d0 = random.randint(0, patch_src_hr.shape[2] - size)
-        patch_src_hr = patch_src_hr[h0:h0 + size, w0:w0 + size, d0:d0 + size]
+        patch_src_hr = patch_src_hr[h0:h0 + size, w0:w0 + size, d0:d0 + size] #Obtain high-resolution patches from the extracted volumes
         patch_tgt_hr = patch_tgt_hr[h0:h0 + size, w0:w0 + size, d0:d0 + size]
-        patch_src_lr = zoom(patch_src_hr, 1 / s)
+        patch_src_lr = zoom(patch_src_hr, 1 / s) # ownsample each high-resolution patch to a fixed size of 20×20×20 to ensure consistent input dimensions
         patch_tgt_lr = zoom(patch_tgt_hr, 1 / s)
         coord_hr = utils.make_coord(patch_src_hr.shape, flatten=True)
         patch_src_hr = patch_src_hr.reshape(-1, 1)
