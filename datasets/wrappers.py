31395bec5c8287d3cc43d10a5139aa9399dab3c6
kellyWang <57870393+Wangyulin-user@users.noreply.github.com>
2025-04-04 17:40:29 +0800

    Update wrappers.py

--- datasets/wrappers.py
+++ datasets/wrappers.py
@@ -36,13 +36,13 @@ class SRImplicitPaired(Dataset):
         d0 = random.randint(0, patch_src_hr.shape[2] - size)
         patch_src_hr = patch_src_hr[h0:h0 + size, w0:w0 + size, d0:d0 + size] #Obtain high-resolution patches from the extracted volumes
         patch_tgt_hr = patch_tgt_hr[h0:h0 + size, w0:w0 + size, d0:d0 + size]
-        patch_src_lr = zoom(patch_src_hr, 1 / s) # ownsample each high-resolution patch to a fixed size of 20×20×20 to ensure consistent input dimensions
+        patch_src_lr = zoom(patch_src_hr, 1 / s) # Downsample each high-resolution patch to a fixed size of 20×20×20 to ensure consistent input dimensions
         patch_tgt_lr = zoom(patch_tgt_hr, 1 / s)
-        coord_hr = utils.make_coord(patch_src_hr.shape, flatten=True)
-        patch_src_hr = patch_src_hr.reshape(-1, 1)
+        coord_hr = utils.make_coord(patch_src_hr.shape, flatten=True) # Make coordinates at grid centers.
+        patch_src_hr = patch_src_hr.reshape(-1, 1) # Flatten the high-resolution patch so that its shape matches the coordinate grid
         patch_tgt_hr = patch_tgt_hr.reshape(-1, 1)
 
-        if self.sample_q is not None:
+        if self.sample_q is not None: # Select the target number of pixels and their coordinates from the high-resolution patch for prediction
             sample_indices = np.random.choice(len(coord_hr), self.sample_q, replace=False)
             coord_hr = coord_hr[sample_indices]
             patch_src_hr = patch_src_hr[sample_indices]
